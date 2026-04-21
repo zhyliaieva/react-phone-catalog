0bd1e1b59e759dc932c47b891e21abeac20eef8a
Dina Zhyliaieva <din.zhyliaieva@gmail.com>
2026-04-21 10:15:10 +0300

    Move all code

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,70 @@
-import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import HomePage from './modules/HomePage/index';
+import ProductsPage from './modules/ProductsPage';
+import ProductDetailsPage from './modules/ProductDetailsPage/index';
+import CartPage from './modules/CartPage/index';
+import FavoritesPage from './modules/FavoritesPage/index';
+import { NotFoundPage } from './modules/NotFoundPage';
+import Header from './components/Header';
+import Footer from './components/Footer/index';
 import './App.scss';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
+const App = () => {
+  const onBackToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
 
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
+    <>
+      <div className="app-container">
+        <Header />
+        <main className="main">
+          <div className="main__content">
+            <h1 className="visually-hidden">Product Catalog</h1>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/phones">
+                <Route
+                  index
+                  element={
+                    <ProductsPage category="phones" title="Phones page" />
+                  }
+                />
+                <Route path=":productId" element={<ProductDetailsPage />} />
+              </Route>
+              <Route path="/tablets">
+                <Route
+                  index
+                  element={
+                    <ProductsPage category="tablets" title="Tablets page" />
+                  }
+                />
+                <Route path=":productId" element={<ProductDetailsPage />} />
+              </Route>
+              <Route path="/accessories">
+                <Route
+                  index
+                  element={
+                    <ProductsPage
+                      category="accessories"
+                      title="Accessories page"
+                    />
+                  }
+                />
+                <Route path=":productId" element={<ProductDetailsPage />} />
+              </Route>
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/favorites" element={<FavoritesPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </div>
+        </main>
+
+        <Footer onBackToTop={onBackToTop} />
+      </div>
+    </>
   );
 };
+
+export default App;
